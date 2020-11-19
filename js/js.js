1d1c3b8f90f9d1a42b9e944b675562caf8f64ae1
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2020-11-19 14:38:38 -0300

    Andamento

---
+++ js/js.js
@@ -0,0 +1,10 @@
+
+function start() { // Inicio da função start()
+
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+    $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+}
